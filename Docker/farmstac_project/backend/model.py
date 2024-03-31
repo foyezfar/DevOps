be7ac283a871d37ca69db942b064c4425c8e492a
Foyez <foyez.far@gmail.com>
2024-04-01 00:22:22 +0600

    Add env folder to .gitignore

--- Docker/farmstac_project/backend/model.py
+++
@@ -1,9 +0,0 @@
-#  @bekbrace
-#  FARMSTACK Tutorial - Sunday 13.06.2021
-
-# Pydantic allows auto creation of JSON Schemas from models
-from pydantic import BaseModel
-
-class Todo(BaseModel):
-    title: str
-    description: str
